2e55203ddb64378276be4db1e922d983f67400d2
aishwaryapb <aishbannimatti@gmail.com>
2021-04-08 00:09:41 +0530

    Added conversion logic and updated styling

--- src/components/StyleConverter.vue
+++ src/components/StyleConverter.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="wrapper">
     <div class="editors">
-      <prism-editor class="editor" v-model="reactStyle" :highlight="jsHighlighter" line-numbers></prism-editor>
-      <prism-editor class="editor" v-model="css" :highlight="cssHighlighter" line-numbers readonly></prism-editor>
+      <div class="section">
+        <h2>React style json</h2>
+        <prism-editor class="editor" v-model="reactStyle" :highlight="jsHighlighter" line-numbers></prism-editor>
+      </div>
+      <div class="section">
+        <h2>css</h2>
+        <prism-editor class="editor" v-model="css" :highlight="cssHighlighter" line-numbers readonly></prism-editor>
+      </div>
     </div>
-    <button>Convert</button>
+    <button @click="convert()">Convert</button>
   </div>
   
 </template>
@@ -12,24 +18,45 @@
 <script>  
   import { highlight, languages } from 'prismjs/components/prism-core';
   import 'prismjs/components/prism-clike';
-  import 'prismjs/components/prism-javascript';
+  import 'prismjs/components/prism-json';
   import 'prismjs/themes/prism-tomorrow.css';
   import 'prismjs/components/prism-css';
+
+  const camelToKebabCase = str => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
   
 export default {
   name: 'HelloWorld',
   data: function() {
     return {
-      reactStyle: "console.log('Hello World');",
+      reactStyle: '{\n\twidth: "25px",\n\theight: "25px",\n\tmargin: "auto 0",\n\tbackgroundColor: "rgba(128, 134, 147, 0.2)",\n\tborderRadius: "10px",\n}',
       css: "/* Converted css here */"
     }
   },
   methods: {
     jsHighlighter(code) {
-      return highlight(code, languages.js, "js");
+      return highlight(code, languages.json, "json");
     },
     cssHighlighter(code) {
       return highlight(code, languages.css, "css");
+    },
+    convert() {
+      const splitStyle = this.reactStyle.split(":");
+      let css = "/* Converted css here */\n\n"
+      const re = "\\w+$";
+      splitStyle.forEach((bit, index) => {
+        if(index === 0) {
+          const prop = bit.match(re)?.[0];
+          prop && (css += `${camelToKebabCase(prop)}: `);
+        }
+        else {
+          const secondSplit = bit.split('"');
+          css += `${secondSplit[1]};\n`;
+          const nextProp = secondSplit[2].match(re)?.[0];
+          nextProp && (css += `${camelToKebabCase(nextProp)}: `);
+        }
+      });
+
+      this.css = css;
     }
   }
 }
@@ -49,11 +76,23 @@ export default {
   width: 100%;
 }
 
-.editor {
+.section {
   width: 50%;
-  height: 60vh;
-  overflow: auto;
   margin: 0 1rem;
+}
+
+.section h2 {
+  text-align: center;
+  display: inline-block;
+  color: white;
+  border-radius: 4px;
+  background-color: #ef0041;
+}
+
+.editor {
+  width: 100%;
+  height: 55vh;
+  overflow: auto;
   background-color: white;
 }
 
